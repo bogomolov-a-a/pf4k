d10e91d52af90625032a9700255364134174cb36
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2021-03-28 19:41:43 +0300

    fixed groupid for subprojects.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,6 @@ allprojects {
     val projectVersion: String by project
     group = projectGroup
     version = projectVersion
-    if (project != rootProject) {
-        project.group = (project.group as String) + project.path.replace("~/:/, ‘.’".toRegex(), "")
-    }
 }
 
 tasks.withType<Wrapper>().configureEach {
